bc2f37815c4b8dfbc0fa334a639106d71ef0c08d
Chibuzo Ekwue <chieezy5@gmail.com>
2023-05-10 22:58:58 +0100

    Added db connection, middleware and auth route handlers

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./utils/db.js";
+import userRoute from "./router/auth.js";
 
 // Middlewares
 const app = express();
 app.use(cors());
+app.use(express.json())
+app.use(cookieParser())
 
 const port = process.env.PORT || 5000;
 
@@ -15,6 +20,10 @@ app.get("/api/health", (req, res) => {
 	});
 });
 
+// users auth route
+app.use("/api/v1/auth",userRoute)
+
 app.listen(port, () => {
+	connectDB();
 	console.log(`Server running on port: ${port}`);
 });
