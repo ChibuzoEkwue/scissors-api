09ab0d46dba0d1c5aeb2597ff42a630d50c50ab3
Chibuzo Ekwue <chieezy5@gmail.com>
2023-05-11 10:28:07 +0100

    Added link route

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./utils/db.js";
 import userRoute from "./router/auth.js";
+import linkRoute from "./router/links.js";
 
 // Middlewares
 const app = express();
 app.use(cors());
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 5000;
 
@@ -21,7 +22,8 @@ app.get("/api/health", (req, res) => {
 });
 
 // users auth route
-app.use("/api/v1/auth",userRoute)
+app.use("/api/v1/auth", userRoute);
+app.use("/api/v1/links", linkRoute);
 
 app.listen(port, () => {
 	connectDB();
