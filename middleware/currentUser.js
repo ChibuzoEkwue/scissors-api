cb04090b770d5c7439e0ce649a8b7f5ac6cbac41
Chibuzo Ekwue <chieezy5@gmail.com>
2023-05-11 10:31:20 +0100

    user middleware

---
+++ middleware/currentUser.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import Users from "../model/Users.js";
+
+const currentUser = (req, res, next) => {
+	try {
+		const token = req.cookies.access_token;
+		if (!token) {
+			throw new Error("You are not authenticated");
+		}
+		jwt.verify(token, process.env.JWT, async (err, user) => {
+			if (err) throw new Error("Invalid token");
+			const currentUser = await Users.findById(user.id);
+            
+			req.user = currentUser;
+			next();
+		});
+	} catch (error) {
+		res.status(401).json({ errorMsg: error.message });
+	}
+};
+
+
+export default currentUser
